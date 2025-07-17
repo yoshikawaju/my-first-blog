e61e65e7a572c8364ea3ae6a3b4f44d5e5a4cd88
yoshikawaju <jun.yoshikawa@onodera-ct.com>
2025-07-17 17:36:06 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+def post_list(request):
+    # ここにビューのロジックを記述します
+    # 例えば、return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {})
